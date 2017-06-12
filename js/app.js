21c17958f1ce8d35162a2a22e94c92dccd870747
dima716 <dima716@yandex.ru>
2017-06-12 19:20:28 +0300

    Add slider block

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 (function(global) {
   'use strict';
   var toggleMenu = document.getElementsByClassName('main-nav__toggler')[0],
-  mainNav = document.getElementsByClassName('main-nav')[0];
+      mainNav = document.getElementsByClassName('main-nav')[0];
 
   toggleMenu.addEventListener('click', function () {
     if ( mainNav.classList.contains('main-nav--open') ) {
@@ -10,4 +10,61 @@
       mainNav.classList.add('main-nav--open');
     }
   });
+
+  function Slider(options) {
+    this.activePaginatorItemClass = options.activePaginatorItemClass;
+    this.activeSliderItemIndex = options.activeSliderItemIndex || 0;
+    this.slider = global.document.querySelector(options.selector);
+  }
+
+  Slider.prototype.init = function (argument) {
+    var sliderInner = this.slider.querySelector('.slider__inner'),
+        sliderItems = sliderInner.querySelectorAll('.slider__item'),
+        slideWidth = sliderItems[0].offsetWidth,
+        sliderPagination = this.slider.querySelector('.slider__pagination'),
+        sliderPaginationItems = sliderPagination.querySelectorAll('.slider__pagination-item'),
+        transformValue = 0;
+
+    function clearSliderPaginationItems() {
+      [].forEach.call(sliderPaginationItems, (function (item) {
+        item.classList.remove(this.activePaginatorItemClass);
+      }).bind(this));
+    }
+
+    [].forEach.call(sliderPaginationItems, function(item, index) {
+      item.setAttribute('data-index', index);
+    });
+
+    sliderInner.style.width = slideWidth * sliderItems.length + 'px';
+
+    sliderPagination.addEventListener('click', (function (event) {
+      let target = event.target;
+
+      while(target != sliderPagination) {
+        if (target.tagName == 'SPAN') {
+          var index = global.parseInt(target.getAttribute('data-index')),
+              moveDistance = Math.abs(index - this.activeSliderItemIndex) * slideWidth;
+
+          clearSliderPaginationItems.call(this);
+          target.classList.add(this.activePaginatorItemClass);
+
+          transformValue = index < this.activeSliderItemIndex ? transformValue + moveDistance : transformValue - moveDistance;
+
+          sliderInner.style.transform = 'translateX(' + transformValue + 'px)';
+          this.activeSliderItemIndex = index;
+          return;
+        }
+
+        target = target.parentNode;
+      }
+    }).bind(this));
+  };
+
+
+  var reviewSlider = new Slider({
+    selector: '.reviews__list',
+    activePaginatorItemClass: 'reviews__pagination-item--active'
+  });
+
+  reviewSlider.init();
 })(window);
