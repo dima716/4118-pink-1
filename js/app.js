d0d59726352fecbd6a80ec2dba7a28265bdd27d1
dima716 <dima716@yandex.ru>
2017-06-15 15:30:17 +0300

    Add contacts.css

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 (function(global) {
   'use strict';
   var toggleMenu = document.getElementsByClassName('main-nav__toggler')[0],
-      mainNav = document.getElementsByClassName('main-nav')[0];
+      mainNav = document.getElementsByClassName('main-nav')[0],
+      geocoder, map;
 
   toggleMenu.addEventListener('click', function () {
     if ( mainNav.classList.contains('main-nav--open') ) {
@@ -62,6 +63,31 @@
     }).bind(this));
   };
 
+  function initMap() {
+    geocoder = new google.maps.Geocoder();
+
+    geocoder.geocode({
+      'address': 'г. Санкт-Петербург, ул.Большая Конюшенная, д. 19/8'
+    }, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map = new google.maps.Map(document.getElementsByClassName('contacts__map')[0], {
+          center: results[0].geometry.location,
+          zoom: 16,
+          scrollwheel: false
+        });
+
+        var image = 'img/icon-map-marker.svg';
+
+        var placeMarker = new google.maps.Marker({
+          position: results[0].geometry.location,
+          map: map,
+          icon: image
+        });
+      }
+    });
+  }
+
+  global.initMap = initMap;
 
   var reviewSlider = new Slider({
     selector: '.reviews__list',
